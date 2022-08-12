f97ff031190c9f5295911e392a6f65e754e475c6
dosia <dosia96@163.com>
2022-08-12 14:53:38 +0800

    init

---
+++ client/clientTest.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+
+let ws = new WebSocket('ws://1.117.115.72:3445/chat');
+
+// 打开WebSocket连接后立刻发送一条消息:
+ws.on('open', function () {
+    console.log(`[CLIENT] open()`);
+    ws.send('Hello!');
+});
+
+// 响应收到的消息:
+ws.on('message', function (message) {
+    console.log(`[CLIENT] Received: ${message}`);
+})
